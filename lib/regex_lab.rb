f7057fa82adcf052e6bde9e083d3cb8224d11e5b
Elliott Vargo <vargoelliott@gmail.com>
2021-01-19 01:25:10 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -12,13 +12,10 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-  if text.scan(/[a-z]{5}/i)
+ 
      new_array = text.scan(/[a-z]{5}/i)
     new_array.split(" ")
     new_array
-  else
-  nil
-  end
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
