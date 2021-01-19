e66d79331b8c3b835c67aa791f56861e01e88513
Elliott Vargo <vargoelliott@gmail.com>
2021-01-19 01:09:28 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,13 +1,22 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/\b[aeiou]\w*/i)
+    return true
+ elsif word.match(/\b[bcdfghjklmnpqrstvwxyz]\w*/i)
+    return false
+ end
 end
+
 
 def words_starting_with_un_and_ending_with_ing(text)
 
 end
 
 def words_five_letters_long(text)
-
+  if text.scan(/[a-z]{5}/i)
+    return text
+  else
+  nil
+  end
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
