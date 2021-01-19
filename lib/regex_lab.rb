73e611928ae3e3a263bf3ea79be575f49b4b6e51
Elliott Vargo <vargoelliott@gmail.com>
2021-01-19 02:52:12 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -24,5 +24,9 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-
+  if phone.match(/\d{10}/) || phone.match(/([(]| )(\d{3})([)]| |-)(\d{3})(-| )(\d{4})\z/)
+    return true
+  else
+    return false
+  end
 end
